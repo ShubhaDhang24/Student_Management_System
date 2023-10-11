7ba3e1441c01868538012c53a0cab3a3b3963b91
Shubha Dhang <shubhadhang@gmail.com>
2023-10-11 09:15:02 +0200

    Initial commit

---
+++ src/main/java/shubhaDhang/App.java
@@ -0,0 +1,13 @@
+package shubhaDhang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
