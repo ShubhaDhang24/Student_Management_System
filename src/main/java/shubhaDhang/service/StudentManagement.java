3e9fd65433e6acc73bdcd5a43542e7e4cfe15a6e
Shubha Dhang <shubhadhang@gmail.com>
2023-10-11 11:40:10 +0200

    implemented service package  injected UserService and studentDao objects in StudentManagementConsoleImpl class

--- src/main/java/shubhaDhang/service/StudentManagement.java
+++ src/main/java/shubhaDhang/service/StudentManagement.java
@@ -6,10 +6,15 @@ import java.util.List;
 
 public interface StudentManagement {
     Student create();
+
     Student save(Student student);
+
     Student find(int id);
+
     Student remove(int id);
+
     List<Student> findAll();
+
     Student edit(Student student);
 
 }
--- src/main/java/shubhaDhang/service/StudentManagementConsoleImpl.java
+++ src/main/java/shubhaDhang/service/StudentManagementConsoleImpl.java
@@ -7,9 +7,9 @@ import shubhaDhang.util.UserInputService;
 
 import java.util.List;
 
-public class StudentManagementConsoleImpl implements  StudentManagement{
-   private UserInputService scannerService;
-    private StudentDao studentDao;
+public class StudentManagementConsoleImpl implements StudentManagement {
+    private final UserInputService scannerService;
+    private final StudentDao studentDao;
 
     public StudentManagementConsoleImpl(UserInputService inputService, ScannerInputService scannerService, StudentDao studentDao) {
         this.scannerService = inputService;
@@ -18,19 +18,19 @@ public class StudentManagementConsoleImpl implements  StudentManagement{
 
     @Override
     public Student create() {
-        String name=scannerService.getString();
-        int id=scannerService.getInt();
-        Student student=new Student(id,name);
+        String name = scannerService.getString();
+        int id = scannerService.getInt();
+        Student student = new Student(id, name);
         return student;
     }
 
     @Override
     public Student save(Student student) {
-        if(student!=null) {
+        if (student != null) {
             studentDao.save(student);
             return student;
         }
-        throw  new RuntimeException("Student is null");
+        throw new RuntimeException("Student is null");
 
     }
 
@@ -49,7 +49,7 @@ public class StudentManagementConsoleImpl implements  StudentManagement{
 
     @Override
     public List<Student> findAll() {
-        List<Student> foundStudents=studentDao.findAll();
+        List<Student> foundStudents = studentDao.findAll();
         return foundStudents;
     }
 
